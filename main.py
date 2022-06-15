242e304bceb680483b404f09c3d9cb8c7b526846
unknown <arix69sex@gmail.com>
2022-06-14 23:11:24 -0500

    Made some progress to auth and seeing how to query twetts. Cant progress more yet because I have to wait to upgrade my twitter development account permissions

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import os
 import tweepy as tw
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-import dotenv as de
+import dotenv
 
-load_dotenv()
+# Setting enviroment variables
+dotenv.load_dotenv(dotenv.find_dotenv())
+bearerToekn = os.getenv("BEARER_TOKEN")
+consumerKey = os.getenv("API_KEY")
+consumerSecret =  os.getenv("API_SECRET")
+accessToken = os.getenv("ACCESS_TOKEN")
+accessSecret =  os.getenv("ACCESS_TOKEN_SECRET")
+
+# Authentification
+auth = tw.OAuthHandler(consumer_key=consumerKey, consumer_secret=consumerSecret)
+auth.set_access_token(accessToken,accessSecret)
+#api = tw.API(auth, wait_on_rate_limit=True)
+api = tw.Client(bearer_token=bearerToekn, consumer_key=consumerKey, consumer_secret=consumerSecret, access_token=accessToken, access_token_secret=accessSecret)
+
+# Set up query for twitter data
+hashtag = '#cryptocurrency'
+query = tw.Cursor(api.search_recent_tweets, q=hashtag).items(100)
+tweets = [{'Tweet': tweet.text, 'Timestamp': tweet.created_at} for tweet in query] 
+print(tweets)
+
